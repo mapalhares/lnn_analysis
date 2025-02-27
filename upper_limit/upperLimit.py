7bcd198ad27ede97e15abe0d83212d2f73813c3c
Francesco Mazzaschi <fmazzasc@alipap1.cern.ch>
2025-02-27 10:27:10 +0100

    Fix upper limit + add qa histograms

--- upper_limit/upperLimit.py
+++ upper_limit/upperLimit.py
@@ -4,10 +4,11 @@ from ROOT import RooFit, RooRealVar, RooDataHist, RooArgList, RooArgSet, RooGaus
 # Open the ROOT file
 file = ROOT.TFile.Open("InvariantMass_0_50_4.5_pT_8.0_run_342361.root")
 hMass = file.Get("hMass_0_50_PtRange_4.5_8.0")
+hMass.SetDirectory(0)
+file.Close()
 
 # Define the invariant mass variable
 mass = RooRealVar("mass", "Invariant Mass", hMass.GetXaxis().GetXmin(), hMass.GetXaxis().GetXmax(), "GeV/c^{2}")
-
 # Create a RooDataHist from the histogram
 data = RooDataHist("data", "Dataset with mass", RooArgList(mass), hMass)
 
@@ -21,37 +22,24 @@ gauss = RooGaussian("gauss", "Gaussian Signal", mass, mean, sigma)
 mean.setConstant(True)
 sigma.setConstant(True)
 
-# x_min = 2.987
-# x_max = 2.993
-    
-# sigma = ROOT.RooRealVar("sigma", "sigma", s, -5*s, 5*s)
-# mean = ROOT.RooRealVar("mean", "mean", m, m - 5*s, m+5*s)
-# alphaL = ROOT.RooRealVar("alphaL", "alphaL", 2.0, -5, 5)  # Aumentar rigidez da cauda esquerda
-# nL = ROOT.RooRealVar("nL", "nL", 5, 0, 1000)                # Aumentar decaimento da cauda esquerda
-# alphaR = ROOT.RooRealVar("alphaR", "alphaR", 2., -10, 10)  # Aumentar rigidez da cauda direita
-# nR = ROOT.RooRealVar("nR", "nR", 9, 0, 1000)     
-        
-# cbds = ROOT.RooCrystalBall("cbds", "Crystal Ball", mass, mean, sigma, alphaL, nL, alphaR, nR)
 
 # Define the background polynomial (2nd order)
-a0 = RooRealVar("a0", "a0", 0, -10, 10)  # Valores menores
-a1 = RooRealVar("a1", "a1", 0, -10, 10)
-a2 = RooRealVar("a2", "a2", 0, -10, 10)
-background = RooChebychev("background", "Polynomial Background", mass, RooArgList(a0, a1, a2))
+a0 = RooRealVar("a0", "a0",  -10, 10)  # Valores menores
+a1 = RooRealVar("a1", "a1",  -10, 10)
+background = RooChebychev("background", "Polynomial Background", mass, RooArgList(a0, a1))
 
 # a0.setConstant(True)
 # a1.setConstant(True)
 # a2.setConstant(True)
 
 # Define the fractions of signal and background
-nsig = RooRealVar("nsig", "Number of signal events", 100, 0, 10000)
-nbkg = RooRealVar("nbkg", "Number of background events", 1000, 0, 100000)
+nsig = RooRealVar("nsig", "Number of signal events", 100, 0, 1000000)
+nbkg = RooRealVar("nbkg", "Number of background events", 1000, 0, 1000000)
 
 # Create the total model
 model = RooAddPdf("model", "Signal + Background", RooArgList(gauss, background), RooArgList(nsig, nbkg))
 
-# Fit the model to the data
-result = model.fitTo(data, RooFit.Save(), RooFit.Strategy(1))
+
 
 # Plot the data and the fit result
 frame = mass.frame()
@@ -61,9 +49,6 @@ model.plotOn(frame, RooFit.Name("background"), RooFit.Components("background"), 
 model.plotOn(frame, RooFit.Name("gauss"), RooFit.Components("gauss"), RooFit.LineColor(ROOT.kRed-4), RooFit.LineStyle(ROOT.kSolid), RooFit.LineWidth(3))
 model.paramOn(frame)
 
-
-# Fechar o arquivo ROOT de entrada
-file.Close()
 
 # Create a workspace
 workspace = ROOT.RooWorkspace("workspace")
@@ -79,7 +64,7 @@ model_config = RooStats.ModelConfig("model_config", workspace)
 model_config.SetPdf(model)
 model_config.SetParametersOfInterest(nsig)
 model_config.SetObservables(RooArgSet(mass))
-model_config.SetNuisanceParameters(RooArgSet(a0, a1, a2, nbkg))
+model_config.SetNuisanceParameters(RooArgSet(a0, a1, nbkg))
 oldValue = nsig.getVal()
 
 # Create a null (background-only) model
@@ -89,7 +74,7 @@ nsig.setVal(0)
 workspace.var("nsig").setVal(0)
 null_model.SetParametersOfInterest(nsig)
 null_model.SetObservables(RooArgSet(mass))
-null_model.SetNuisanceParameters(RooArgSet(a0, a1, a2, nbkg))
+null_model.SetNuisanceParameters(RooArgSet(a0, a1, nbkg))
 null_model.SetSnapshot(RooArgSet(nsig))
 null_model.GetParametersOfInterest().first().setVal(oldValue)
 print("Null model snapshot:")
@@ -119,7 +104,44 @@ low_limit = result.LowerLimit()
 error_up = result.UpperLimitEstimatedError()
 error_lower = result.LowerLimitEstimatedError()
 
+result = model.fitTo(data, RooFit.Save(), RooFit.Strategy(1))
+
+## set signal to 0
+nsig.setVal(0)
+# Plot the data and the fit result
+frame_signal_null = mass.frame()
+frame_signal_null.SetName("frame_signal_null")
+model.plotOn(frame_signal_null, RooFit.Name("model"))
+
+## set signal to upper limit
+nsig.setVal(upper_limit)
+frame_signal = mass.frame()
+frame_signal.SetName("frame_signal")
+model.plotOn(frame_signal, RooFit.Name("model"))
+
+
+# Plot the data and the fit result
+# Fit the model to the data
+frame = mass.frame()
+frame.SetName("frame_data")
+data.plotOn(frame, RooFit.Name("data"))
+model.plotOn(frame, RooFit.Name("model"))
+model.plotOn(frame, RooFit.Name("background"), RooFit.Components("background"), RooFit.LineColor(ROOT.kCyan), RooFit.LineStyle(ROOT.kDashed), RooFit.LineWidth(3))
+model.plotOn(frame, RooFit.Name("gauss"), RooFit.Components("gauss"), RooFit.LineColor(ROOT.kRed-4), RooFit.LineStyle(ROOT.kSolid), RooFit.LineWidth(3))
+model.paramOn(frame)
+
+
+## save to file
+file = ROOT.TFile("data_signal.root", "RECREATE")
+frame_signal.Write()
+frame_signal_null.Write()
+frame.Write()
+file.Close()
+
+print("-------------------------------------------------------------------------------")
 print(f"Upper limit at {confidence_level*100:.0f}% CL: {upper_limit:.2f} +- {error_up}")
+
+
 # nsig_values = []
 # nsig_values.append(upper_limit)
 # nll_values = []
